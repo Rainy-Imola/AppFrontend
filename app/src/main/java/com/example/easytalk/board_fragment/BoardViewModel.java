9ec356f85cbecf981a32a882fcdb1efadb6421d6
RidiculousDoge <313865185@qq.com>
2021-04-24 19:26:49 +0800

    add item_messageboard.xml

--- app/src/main/java/com/example/easytalk/board_fragment/BoardViewModel.java
+++ app/src/main/java/com/example/easytalk/board_fragment/BoardViewModel.java
@@ -2,6 +2,12 @@ package com.example.easytalk.board_fragment;
 
 import androidx.lifecycle.ViewModel;
 
+import com.example.easytalk.Message;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoardViewModel extends ViewModel {
     // TODO: Implement the ViewModel
+    List<Message> MessageViewModel=new ArrayList<>() ;
 }
