dbdb62dac01898836e83859910f25f3bc531887a
Snowfall99 <445117445@qq.com>
2021-05-02 15:45:43 +0800

    fix toast in subthread

--- app/src/main/java/com/example/easytalk/RegisterActivity.java
+++ app/src/main/java/com/example/easytalk/RegisterActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -184,7 +185,9 @@ public class RegisterActivity extends AppCompatActivity {
                                     }
 
                                 } else {
+                                    Looper.prepare();
                                     Toast.makeText(RegisterActivity.this, "Wrong CAPTCHA", Toast.LENGTH_SHORT).show();
+                                    Looper.loop();
                                 }
                             } catch (JSONException e) {
                                 e.printStackTrace();
