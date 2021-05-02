1d22caa528d7a5b92013a9326f97e522981435c2
Snowfall99 <445117445@qq.com>
2021-05-02 15:48:22 +0800

    fix baseurl

--- app/src/main/java/com/example/easytalk/Constants.java
+++ app/src/main/java/com/example/easytalk/Constants.java
@@ -1,6 +1,6 @@
 package com.example.easytalk;
 
 public class Constants {
-    public static final String baseUrl = "http://10.0.2.2:8000";
+    public static final String baseUrl = "http://47.103.123.145";
     public enum State {SUCCESS, FAILURE, NETWORK_FAILURE};
 }
