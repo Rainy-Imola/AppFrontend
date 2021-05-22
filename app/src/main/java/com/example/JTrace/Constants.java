39a1189624bf8d4d91fa9903c72e6f906db7c1a5
Snowfall99 <445117445@qq.com>
2021-05-22 17:07:18 +0800

    random 20 messages

--- app/src/main/java/com/example/JTrace/Constants.java
+++ app/src/main/java/com/example/JTrace/Constants.java
@@ -1,7 +1,7 @@
 package com.example.JTrace;
 
 public class Constants {
-    public static final String baseUrl = "http://47.103.123.145";
+    public static final String baseUrl = "http://10.0.2.2:8000";
     public static final String pictureUrl = "http://106.12.182.249:8000";
     public static final String ServerPubulicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCPU6MZ87K1L78DCG7XgVz5NQwNfHSRT5C7z6SjvP+voGZt62L53XDSe7X/B2xXzS4aeoX/AfLyX6/D718FZ2e2zTrrDrGUc5x3K8yHv+k4lVdKPmaqmvm587vX41cfdojMLyYo1VliPhG8glpFzl+3ByRhK/EOx/BO8Ic+f5VWSQIDAQAB";
     public enum State {SUCCESS,FAILURE,NETWORK_FAILURE};
