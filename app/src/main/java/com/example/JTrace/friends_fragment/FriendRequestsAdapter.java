3adc5c783e2bc22f196912181669e60a8ae01fc8
RidiculousDoge <313865185@qq.com>
2021-05-23 15:22:43 +0800

    finished add FriendRequests mode, problems remaining

--- app/src/main/java/com/example/JTrace/friends_fragment/FriendRequestsAdapter.java
+++ app/src/main/java/com/example/JTrace/friends_fragment/FriendRequestsAdapter.java
@@ -10,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.JTrace.Constants;
+import com.example.JTrace.HttpAPI;
 import com.example.JTrace.R;
 import com.example.JTrace.model.NewFriendMsg;
 import com.example.JTrace.model.NewFriendMsgs;
@@ -20,16 +21,19 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import android.os.Handler;
+import android.widget.Toast;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-
+//TODO:耦合严重！
 public class FriendRequestsAdapter extends RecyclerView.Adapter<FriendRequestsViewHolder> {
     private NewFriendMsgs msgs=new NewFriendMsgs();
     private SharedPreferences sharedPreferences;
+    private Handler mHandler=new Handler();
     @NonNull
     @NotNull
     @Override
@@ -44,7 +48,7 @@ public class FriendRequestsAdapter extends RecyclerView.Adapter<FriendRequestsVi
                     public void run(){
                         OkHttpClient okHttpClient = new OkHttpClient();
                         Request request = new Request.Builder().url(Constants.baseUrl + "/users/"+
-                                viewHolder.authorView.getText()+"/info/")
+                                viewHolder.getFromUsr()+"/info/")
                                 .addHeader("Authorization", sharedPreferences.getString("token",""))
                                 .build();
                         okHttpClient.newCall(request).enqueue(new Callback() {
@@ -64,7 +68,7 @@ public class FriendRequestsAdapter extends RecyclerView.Adapter<FriendRequestsVi
                                     int id=mid.getInt("id");
                                     Intent intent=new Intent(v.getContext(), FriendDetailActivity.class);
                                     intent.putExtra("id",id);
-                                    intent.putExtra("name", viewHolder.authorView.getText());
+                                    intent.putExtra("name", viewHolder.getFromUsr());
                                     v.getContext().startActivity(intent);
                                 } catch (JSONException e) {
                                     e.printStackTrace();
@@ -76,12 +80,101 @@ public class FriendRequestsAdapter extends RecyclerView.Adapter<FriendRequestsVi
                 }.start();
             }
         });
+        viewHolder.addView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HttpAPI httpAPI=new HttpAPI();
+                JSONObject jsonObject=new JSONObject();
+                try{
+                    jsonObject.put("username1",viewHolder.getFromUsr());
+                    jsonObject.put("username2",sharedPreferences.getString("username",null));
+                    //同意加好友，status=1
+                    jsonObject.put("status",1);
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+
+                try {
+                    httpAPI.postApi_withToken(jsonObject, "/friends/response/", new Callback() {
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+                            Runnable runnable=new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(v.getContext(), "加好友请求发送失败！",Toast.LENGTH_SHORT).show();
+                                }
+                            };
+                            mHandler.post(runnable);
+                        }
+
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+                            Runnable runnable=new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(v.getContext(), "添加成功",Toast.LENGTH_SHORT).show();
+                                    viewHolder.bind(viewHolder.getFromUsr(),viewHolder.getContent(),1);
+                                }
+                            };
+                            mHandler.post(runnable);
+                        }
+                    },sharedPreferences.getString("token",null));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        viewHolder.declineView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HttpAPI httpAPI=new HttpAPI();
+                JSONObject jsonObject=new JSONObject();
+                try{
+                    jsonObject.put("username1",viewHolder.getFromUsr());
+                    jsonObject.put("username2",sharedPreferences.getString("username",null));
+                    //同意加好友，status=1
+                    jsonObject.put("status",2);
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+
+                try {
+                    httpAPI.postApi_withToken(jsonObject, "/friends/response/", new Callback() {
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+                            Runnable runnable=new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(v.getContext(), "拒绝加好友请求发送失败！",Toast.LENGTH_SHORT).show();
+                                }
+                            };
+                            mHandler.post(runnable);
+                        }
+
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+                            Runnable runnable=new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(v.getContext(), "已拒绝",Toast.LENGTH_SHORT).show();
+                                    viewHolder.bind(viewHolder.getFromUsr(),viewHolder.getContent(),2);
+                                }
+                            };
+                            mHandler.post(runnable);
+                        }
+                    },sharedPreferences.getString("token",null));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
         return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull @NotNull FriendRequestsViewHolder holder, int position) {
-        holder.bind(msgs.getMsgByIndex(position).getFrom_author_name(),msgs.getMsgByIndex(position).getReqMsg());
+        holder.bind(msgs.getMsgByIndex(position).getFrom_author_name(),msgs.getMsgByIndex(position).getReqMsg()
+        ,msgs.getMsgByIndex(position).getStatus());
     }
 
     @Override
--- app/src/main/java/com/example/JTrace/friends_fragment/FriendRequestsViewHolder.java
+++ app/src/main/java/com/example/JTrace/friends_fragment/FriendRequestsViewHolder.java
@@ -18,10 +18,13 @@ import java.net.URI;
 
 public class FriendRequestsViewHolder extends RecyclerView.ViewHolder {
     //TODO:为了在adapter中使用，去耦方便，这里都设成了public，代码不优雅
-    public ImageView avatarView,addView,declineView;
+    public ImageView avatarView,addView,declineView,respondedView;
     public TextView authorView;
     public TextView msgView;
     public LinearLayout mainLayout;
+    private String fromUsr;
+    private String content;
+    private int Status;
     public FriendRequestsViewHolder(@NonNull @NotNull View itemView) {
         super(itemView);
         avatarView=itemView.findViewById(R.id.request_avatarView);
@@ -30,11 +33,31 @@ public class FriendRequestsViewHolder extends RecyclerView.ViewHolder {
         addView=itemView.findViewById(R.id.request_addImageView);
         declineView=itemView.findViewById(R.id.request_declineImageView);
         mainLayout=itemView.findViewById(R.id.request_mainLayout);
+        respondedView=itemView.findViewById(R.id.request_respondedImageView);
     }
     //TODO: get avatar and set avatarURI
-    public void bind(String author, String msg){
+    public void bind(String author, String msg,int status){
         //avatarView.setImageURI(avatarURI);
+        fromUsr=author;
+        content=msg;
         authorView.setText(author);
         msgView.setText(msg);
+        Status=status;
+        if(Status!=0){
+            respondedView.setVisibility(View.VISIBLE);
+            addView.setVisibility(View.INVISIBLE);
+            addView.setClickable(false);
+            declineView.setVisibility(View.INVISIBLE);
+            declineView.setClickable(false);
+        }else{
+            respondedView.setVisibility(View.INVISIBLE);
+            addView.setVisibility(View.VISIBLE);
+            addView.setClickable(true);
+            declineView.setVisibility(View.VISIBLE);
+            declineView.setClickable(true);
+        }
     }
+    public String getFromUsr(){return fromUsr;}
+    public String getContent(){return content;}
+    public int getStatus(){return Status;}
 }
